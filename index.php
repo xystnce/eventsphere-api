d10c3b2a9bf7aad04482389a1326e38740e27b52
iTaNonG <perezethan306@gmail.com>
2026-05-03 12:49:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+ 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+ 
 require_once __DIR__ . '/api/api.php';
+ 
